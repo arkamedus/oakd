22a86b54b80fcfdf8335db9781d419a118348871
Gordon Goodrum <stopgordy@gmail.com>
2025-02-09 01:24:30 -0600

    Workflow

--- jest.config.js
+++ jest.config.js
@@ -11,5 +11,9 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
         "identity-obj-proxy",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
-  }
+  },
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "./", outputName: "junit.xml" }]
+  ]
 };
