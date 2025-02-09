7e55dc677671706587c7c9d947bf26b46c2a5725
Gordon Goodrum <stopgordy@gmail.com>
2025-02-09 01:41:54 -0600

    Workflow

--- jest.config.js
+++ jest.config.js
@@ -8,9 +8,9 @@ module.exports = {
   },
   testMatch: ["**/*.test.(ts|tsx)"],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-        "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "identity-obj-proxy",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
   },
+  coverageDirectory: "coverage", // Ensures coverage is saved in the `coverage/` folder
   coverageReporters: ["json-summary", "text", "lcov"]
 };
